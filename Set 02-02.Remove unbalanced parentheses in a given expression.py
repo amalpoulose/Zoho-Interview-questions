7bdec3dc6d87cd3a6b8a2ff100d3a42b979f73a6
Amal Poulose <amalpoulose1995@hotmail.com>
2018-03-23 16:43:20 +0530

    Create Set 02-02.Remove unbalanced parentheses in a given expression.py

---
+++ Set 02-02.Remove unbalanced parentheses in a given expression.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+def remove(s,index):
+    s=s[:index]+s[index+1:]
+    return s
+    
+def main():
+         s=raw_input("string : ")
+         start,stop=0,0
+         while s[start:].find('(') != -1:
+             
+             index=s[start:].find('(')+start
+            
+             
+             while s[stop:index].find(')')!=-1:
+                 s=remove(s,s[stop:].find(')')+stop)
+                 index-=1
+              
+             if s[stop:].find(')') == -1:
+                 s=remove(s,index)
+              
+                 
+             else:
+                 stop=s[stop:].find(')')+stop+1
+                 start=index+1
+                
+         while s[stop:].find(')')!= -1:
+             s=remove(s,s[stop:].find(')')+stop)
+             
+         print "formatted string : ",s
+if __name__=="__main__":
+        main()
